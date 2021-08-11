c97e5e005a29536d1d0a11844e491193b65605d9
barun <barunps3@gmail.com>
2021-08-11 09:01:30 +0200

    update

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,17 @@
-FROM apache/airflow:2.1.1-python3.7
+FROM apache/airflow:2.1.2-python3.8
 
+# Install apt packages as root
 USER root
-
 RUN apt-get update && \
     apt-get upgrade -y
 RUN apt-get install -y python3-rtree
-RUN pip install gitpython==3.1.9
+RUN apt-get install -y git
+
+# Install airflow/python packages as airflow user
+USER airflow
 RUN pip install werkzeug==0.16.*
-RUN pip install SQLAlchemy==1.3.15
 
 COPY script/entrypoint.sh /entrypoint.sh
 
-ENTRYPOINT [ "/bin/sh", "-c" ]
-CMD ["/bin/bash", "-c", "echo", "Hello World"]
+ENTRYPOINT [ "/entrypoint.sh" ]
+CMD ["webserver"]
--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -1,5 +1,97 @@
 #!/bin/bash
 
-export AIRFLOW_HOME="/usr/local/airflow"
 export AIRFLOW__CORE__FERNET_KEY=${FERNET_KEY:=$(python -c "from cryptography.fernet import Fernet; FERNET_KEY = Fernet.generate_key().decode(); print(FERNET_KEY)")}
 export ${AIRFLOW__CORE__EXECUTOR:="${EXECUTOR:-Sequential}Executor"}
+export AIRFLOW__CORE__LOAD_EXAMPLES=False
+
+
+
+
+# Install custom python package if requirements.txt is present
+if [ -e "/requirements.txt" ]; then
+    $(command -v pip) install --user -r /requirements.txt
+fi
+
+
+# Other executors than SequentialExecutor drive the need for an SQL database, here PostgreSQL is used
+if [ "$AIRFLOW__CORE__EXECUTOR" != "SequentialExecutor" ]; then
+    # Check if the user has provided explicit Airflow configuration concerning the database
+    if [ -z "$AIRFLOW__CORE__SQL_ALCHEMY_CONN" ]; then
+        # Default values corresponding to the default compose files
+        : "${POSTGRES_HOST:="postgres"}"
+        : "${POSTGRES_PORT:="5432"}"
+        : "${POSTGRES_USER:="airflow"}"
+        : "${POSTGRES_PASSWORD:="airflow"}"
+        : "${POSTGRES_DB:="airflow"}"
+        : "${POSTGRES_EXTRAS:-""}"
+
+        AIRFLOW__CORE__SQL_ALCHEMY_CONN="postgresql+psycopg2://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}${POSTGRES_EXTRAS}"
+        export AIRFLOW__CORE__SQL_ALCHEMY_CONN
+
+        # Check if the user has provided explicit Airflow configuration for the broker's connection to the database
+        if [ "$AIRFLOW__CORE__EXECUTOR" = "CeleryExecutor" ]; then
+            AIRFLOW__CELERY__RESULT_BACKEND="db+postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DB}${POSTGRES_EXTRAS}"
+            export AIRFLOW__CELERY__RESULT_BACKEND
+        fi
+    else
+        if [[ "$AIRFLOW__CORE__EXECUTOR" == "CeleryExecutor" && -z "$AIRFLOW__CELERY__RESULT_BACKEND" ]]; then
+            >&2 printf '%s\n' "FATAL: if you set AIRFLOW__CORE__SQL_ALCHEMY_CONN manually with CeleryExecutor you must also set AIRFLOW__CELERY__RESULT_BACKEND"
+            exit 1
+        fi
+    fi
+fi
+
+
+# CeleryExecutor drives the need for a Celery broker, here Redis is used
+if [ "$AIRFLOW__CORE__EXECUTOR" = "CeleryExecutor" ]; then
+    # Check if the user has provided explicit Airflow configuration concerning the broker
+    if [ -z "$AIRFLOW__CELERY__BROKER_URL" ]; then
+        # Default values corresponding to the default compose files
+        : "${REDIS_PROTO:="redis://"}"
+        : "${REDIS_HOST:="redis"}"
+        : "${REDIS_PORT:="6379"}"
+        : "${REDIS_PASSWORD:=""}"
+        : "${REDIS_DBNUM:="1"}"
+
+        # When Redis is secured by basic auth, it does not handle the username part of basic auth, only a token
+        if [ -n "$REDIS_PASSWORD" ]; then
+        REDIS_PREFIX=":${REDIS_PASSWORD}@"
+        else
+        REDIS_PREFIX=
+        fi
+    fi 
+    AIRFLOW__CELERY__BROKER_URL="${REDIS_PROTO}${REDIS_PREFIX}${REDIS_HOST}:${REDIS_PORT}/${REDIS_DBNUM}"
+    export AIRFLOW__CELERY__BROKER_URL
+fi
+
+
+case "$1" in
+  webserver)
+    printf "Core executor is : %s\n" "$AIRFLOW__CORE__EXECUTOR"
+    airflow db init
+    airflow users create --username admin \
+        --password admin \
+        --firstname admin \
+        --lastname admin \
+        --role Admin \
+        --email admin
+    if [ "$AIRFLOW__CORE__EXECUTOR" = "LocalExecutor" ] || [ "$AIRFLOW__CORE__EXECUTOR" = "SequentialExecutor" ]; then
+      # With the "Local" and "Sequential" executors it should all run in one container.
+      airflow scheduler &
+    fi
+    exec airflow webserver
+    ;;
+  worker|scheduler|flower)
+    printf "Core executor is : %s\n" "$AIRFLOW__CORE__EXECUTOR"
+    # Give the webserver time to run initdb.
+    sleep 10
+    exec airflow celery "$@"
+    ;;
+  version)
+    exec airflow "$@"
+    ;;
+  *)
+    # The command is something like bash, not an airflow subcommand. Just run it in the right environment.
+    exec "$@"
+    ;;
+esac
